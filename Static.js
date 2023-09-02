86227a80e15eb3c6291a3dba0bae46cab5363610
thetkhine <macbook@MacBooks-MacBook-Pro.local>
2023-09-02 23:00:15 +0630

    init

---
+++ Static.js
@@ -0,0 +1,4 @@
+var a = "hello";
+console.log('Type of a ', typeof a);
+var result = a * 3;
+console.log('Result ', result);
---
+++ Static.ts
@@ -0,0 +1,10 @@
+let a = "hello";
+console.log('Type of a ',typeof a);
+
+a = 10;
+console.log('Type of a ',typeof a);
+
+/*
+let result = a * 3;
+console.log('Result ',result);
+*/
